09e1f8f704599d9d4ceba7379a0d2b976b21ef70
Stefano Arrigoni <sarrigoni@gmail.com>
2026-07-28 17:52:40 -0400

    primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="QuintoRepo",                              # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Stefano Arrigoni",                      # Tu nombre
+    author_email="sarrigoni@gmail.com",             # Tu correo electrónico
+    url="https://github.com/sarrigoni-maker/quintorepo",     # URL del proyecto
+)
