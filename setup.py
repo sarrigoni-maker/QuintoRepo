4585b3379961147920420082100f6919f1aacf45
Stefano Arrigoni <sarrigoni@gmail.com>
2026-07-29 11:32:38 -0400

    Corrige paquete y crea version 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="QuintoRepo",                              # Nombre del paquete
-    version="0.1.0",                                # Versión inicial
+    version="0.1.1",                                # Nueva Version
     packages=find_packages(),                       # Paquetes a incluir
     description="Un paquete pip simple de saludo",  # Breve descripción
     author="Stefano Arrigoni",                      # Tu nombre
